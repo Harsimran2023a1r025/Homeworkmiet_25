a51a7121244e9382d15277ee962d3b3afc59afbb
Harsimran2023a1r025 <146611938+Harsimran2023a1r025@users.noreply.github.com>
2023-11-03 11:41:51 +0530

    Add files via upload

---
+++ smallest_of_5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e;
+    printf("Enter the numbers \n");
+    scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+
+    if(a<b && a<c && a<d && a<e)
+    {
+       printf("a is the smallest number");
+    }
+    else if(b<a && b<c && b<d && b<e)
+    {
+        printf("b is the smallest number");
+    }
+    else if(c<a && c<b && c<d && c<e)
+    {
+       printf("c is the smallest number");
+    }
+    else if(d<a && d<b && d<c && d<e)
+    {
+       printf("d is the smallest number");
+    }
+    else 
+    {
+        printf("e is the smallest number");
+    }
+
+    return 0;
+}    
+   
